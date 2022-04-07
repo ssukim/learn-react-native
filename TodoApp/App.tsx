0f2ec29593ad1d70ec6c199b5665d91963b65f03
JeongSu <jeongsu.kim@corretto.io>
2022-04-07 21:35:51 +0900

    practice typescript TodoApp

---
+++ TodoApp/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
+import {StyleSheet} from 'react-native';
+
+const App = () => {
+  const today = new Date();
+  console.log(today);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <DateHead date={today} />
+        <Empty />
+        <AddTodo />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
+export default App;
