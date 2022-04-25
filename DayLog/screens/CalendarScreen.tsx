3d00f9ea314f894f9f54dd4e54d59a3bfdfd0f0f
JeongSu <jeongsu.kim@corretto.io>
2022-04-25 18:52:26 +0900

    animation practice

--- DayLog/screens/CalendarScreen.tsx
+++ DayLog/screens/CalendarScreen.tsx
@@ -1,14 +1,96 @@
-import React from 'react';
-import {useContext} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import LogContext from './contexts/LogContext';
+import React, {useEffect, useRef, useState} from 'react';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
-function CalendarScreen() {
-  const {logs} = useContext(LogContext);
+function FadeInAndOut() {
+  const animation = useRef(new Animated.Value(1)).current;
+
+  const [hidden, setHidden] = useState(false);
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: hidden ? 0 : 1,
+      useNativeDriver: true,
+    }).start();
+  }, [hidden, animation]);
 
   return (
+    <View>
+      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
+      {/* <Button
+        title="FadeIn"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+      <Button
+        title="FadeOut"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 0,
+            useNativeDriver: true,
+          }).start();
+        }}
+      /> */}
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setHidden(!hidden);
+        }}
+      />
+    </View>
+  );
+}
+
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
+
+function CalendarScreen() {
+  return (
     <View style={styles.block}>
-      {/* <Text style={styles.text}>text: {logs}</Text> */}
+      <FadeInAndOut />
+      <SlideLeftAndRight />
     </View>
   );
 }
@@ -19,6 +101,7 @@ const styles = StyleSheet.create({
     padding: 16,
     fontSize: 24,
   },
+  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
 });
 
 export default CalendarScreen;
