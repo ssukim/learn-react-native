9e24186605c1baf6993081c7ee60225b7897a0d1
ssukim <tntntn0930@gmail.com>
2022-06-29 00:09:09 +0900

    stack init

---
+++ ArticleApp/screens/ArticlesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function ArticleScreen() {
+  return (
+    <View>
+      <Text>Articles</Text>
+    </View>
+  );
+}
+
+export default ArticleScreen;
